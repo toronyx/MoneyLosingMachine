Fixed the moving average, adjusted marvin's strategy

bots.py
@@ -16,8 +16,8 @@ class daily_trading_bot:
     their strategy.
     """
 
-    def __init__(self):
-        self.balance = 0
+    def __init__(self, balance=0):
+        self.balance = balance
         self.shares = 0
 
     def buy(self, daily_data, date, number_of_shares):
@@ -58,15 +58,15 @@ class marvin(daily_trading_bot):
     """
 
     def execute_strategy(self, daily_data, date):
-        price = daily_data[daily_data['TradeDate']==date]['close'][-1]
-        # expect errors if we don't give this bot a 60 day head start on data
-        moving_average_period = 60
-        moving_average = np.mean(daily_data[date<=daily_data['TradeDate']]['close'][-moving_average_period::])
-        print('price, moving_average: ', price, moving_average)
-        if price-moving_average < -1:
-            self.buy(daily_data, date, 1)
+        price = daily_data['close'][0]
+        # expect errors if we don't give this bot a 10 day head start on data
+        # in our implementation, the moving average lags behind the market
+        moving_average_period = 10
+        moving_average = np.mean(daily_data['close'][0:moving_average_period])
+        if (price-moving_average < -1) and (self.balance > price):
             # giving this function a return code gives us a way to see what the
             # bot is doing without relying on print statements
+            self.buy(daily_data, date, self.balance//price)
             return 'buy', 1
         elif (price-moving_average > 1) and (self.shares >= 1):
             self.sell(daily_data, date, self.shares)

main.py
@@ -21,17 +21,17 @@ API_key = open('C:/Users/Tom/github/MoneyLosingMachine/key.txt').read()
 ts = TimeSeries(key=API_key, output_format='pandas')
 print('Key accepted')
 
-
+ticker = 'EXC'
 
 """ Read in data """
 
 #user_input = input('Request new data? (y/n)')
-user_input = 'n'
+user_input = 'y'
 if user_input == 'y':
     print('Requesting Alpha Vantage data...')
     t0 = time.time()
 
-    data, metadata = ts.get_daily('CNCR', outputsize='full')
+    data, metadata = ts.get_daily(ticker, outputsize='full')
     #print(metadata)
     data.to_pickle('data.pkl')
 
@@ -49,7 +49,7 @@ data['time'] = data.index.time
 my_style  = mpf.make_mpf_style(base_mpf_style='nightclouds',marketcolors=my_colors)
 mpf.plot(data[data['TradeDate']>datetime.date(2021, 1, 1)],
          type='candle', style=my_style, mav=(7,21,50), volume=True, show_nontrading=True,
-         title='CNCR candle plot')"""
+         title='{0} candle plot'.format(ticker))"""
 
 
 
@@ -58,29 +58,29 @@ mpf.plot(data[data['TradeDate']>datetime.date(2021, 1, 1)],
 plt.style.use('dark_background')
 fig, ax = plt.subplots(nrows=1, ncols=1)
 ax.plot(data[data['TradeDate']>datetime.date(2021, 1, 1)]['close'])
-ax.set_title('Close price for CNCR ticker')
+ax.set_title('Close price for {0} ticker'.format(ticker))
 ax.set_ylabel('Close price /\$')
 
-current_bot = bots.marvin() #simple_jack()
+current_bot = bots.marvin(balance=100) #simple_jack()
+
 #for date in pd.date_range(start="2020-01-01",end="2020-07-01"):
 action_info = np.empty((0,3))
 moving_average = np.empty(0)
-moving_average_period = 60
+moving_average_period = 10
 trading_dates = []
 for date in pd.date_range(start="2021-01-01", end=data['TradeDate'].max()):
     if not data[data['TradeDate']==date].empty:
         action = current_bot.execute_strategy(data[data['TradeDate']<=date], date)
-        #price = daily_data[daily_data['TradeDate']==date]['close'][-1]
         action_info = np.vstack((action_info, np.array([date, action[0], action[1]])))
         trading_dates.append(date)
 
-        """ Authors note: since we're just using the regular index, if there's a
-        day without trades, it will not be counted in the moving average (or maybe it will, check the indentation).
-        This could cause weird behaviour later. """
-        moving_average = np.append(moving_average, np.mean(data[date<=data['TradeDate']]['close'][-moving_average_period::]))
-        print('moving average:', moving_average[-1])
+        # Note: keep in mind we are skipping days with no trades
+        moving_average = np.append(moving_average, np.mean(data[data['TradeDate']<=date]['close'][0:moving_average_period]))
 
-print('Total profit: $ {0:.2f}'.format(current_bot.balance))
+final_info = ('Cash balance: $ {0:.2f}, Stocks owned: {1:.0f} \n'
+              +'Balance (inc. stocks): $ {2:.2f}').format(current_bot.balance, current_bot.shares,
+                                                 current_bot.balance+current_bot.shares*data['close'][0])
+print(final_info)
 
 buy_dates = action_info[:,0][np.where(action_info[:,1]=='buy')[0]]
 sell_dates = action_info[:,0][np.where(action_info[:,1]=='sell')[0]]
@@ -89,6 +89,6 @@ ax.scatter(buy_dates, data.loc[buy_dates]['close'], marker='^', color='g', label
 ax.scatter(sell_dates, data.loc[sell_dates]['close'], marker='v', color='r', label='sell')
 #ax.plot(pd.date_range(start="2021-01-01", end=data['TradeDate'].max()), moving_average, label='moving average')
 ax.plot(trading_dates, moving_average, label='moving average')
-ax.legend(title='Total profit: $ {0:.2f}'.format(current_bot.balance))
+ax.legend(title=final_info)
 
 plt.show()